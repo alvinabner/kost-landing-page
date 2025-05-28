0e8864b4b398fdf6677931b55601517ae99a093f
alvinabner <alvinabnert@gmail.com>
2025-05-28 14:59:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 240) {
+        navbar.classList.add('scrolled');
+        navbar.classList.remove('top-10');
+        navbar.classList.add('top-0'); 
+    } else {
+        navbar.classList.remove('scrolled');
+        navbar.classList.add('top-10'); 
+        navbar.classList.remove('top-0'); 
+    }  
+});
